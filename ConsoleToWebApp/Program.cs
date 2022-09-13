5c691b15763619721b1f3734f3a511be7feb6056
Riku Morishima <riku.morishima@gmail.com>
2022-09-13 15:07:43 -0500

    Add project files.

---
+++ ConsoleToWebApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using ConsoleToWebApp;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        CreateHostBuilder(args).Build().Run();
+    }
+
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+        .ConfigureWebHostDefaults(webuilder =>
+        {
+            webuilder.UseStartup<Startup>();
+        });
+}
---
+++ ConsoleToWebApp/Startup.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleToWebApp
+{
+    public class Startup
+    {
+        public void ConfigurationServices(IServiceCollection services)
+        {
+
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseRouting();
+            app.UseEndpoints(endpoint =>
+            {
+                endpoint.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Hello world!");
+                });
+                endpoint.MapGet("/echo/{id}", async (HttpContext context, string id) =>
+                {
+                    await context.Response.WriteAsync($"Hello {id}!");
+                });
+            });
+        }
+    }
+}
